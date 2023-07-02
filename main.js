d8c6983f8af71b522523e6273fe44006b9c60691
Gingene <ginx2618@gmail.com>
2023-07-02 09:17:04 +0800

    add bootstrap style

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
+console.time();
 import "./assets/scss/all.scss";
+console.timeEnd();
+
+import "./assets/scss/all.scss";
+import "./node_modules/bootstrap/js/index.umd";
 
 const testH2 = document.querySelector("#testH2");
-const btn = document.querySelector(".btn");
+const add1B = document.querySelector("#add1");
 
 let number = 0;
-btn.addEventListener("click", () => {
+add1B.addEventListener("click", () => {
   number += 1;
   testH2.innerText = number;
 });
